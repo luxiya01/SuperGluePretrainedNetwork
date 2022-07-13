cb32f17e329cd8004f3cb28ec5936cc9884b92e2
Li Ling <liling@kth.se>
2022-07-13 12:50:34 +0200

    Add file to test training procedure

---
+++ train.py
@@ -0,0 +1,26 @@
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import WandbLogger
+from models.matching_train import MatchingTrain
+from data.ssspatch_datamodule import SSSPatchDataModule
+
+wandb_logger = WandbLogger(project='sss-corr', name='220603_testrun_superglue+SIFT_mean_loss')
+model = MatchingTrain(config={'descriptor_dim': 128, 
+        'keypoint_encoder': [32, 64, 128]})
+#TODO: use GPU
+#trainer = Trainer(logger=wandb_logger, accelerator='gpu', devices=1, max_epochs=10)
+trainer = Trainer(logger=wandb_logger, max_epochs=5)
+
+ssspatch_sift_norm_img = SSSPatchDataModule(
+    root=
+    '/home/li/Documents/sss-correspondence/data/GullmarsfjordSMaRC20210209_ssh_annotations/survey2_better_resolution/9-0169to0182-nbr_pings-1301_annotated/patch240_step40_test0.1_refSSH-0170/',
+    desc='sift',
+    img_type='norm',
+    min_overlap_percentage=0.15,
+    eval_split=.1,
+    batch_size=1,
+    num_workers=8)
+ssspatch_sift_norm_img.setup()
+print(f'Len train: {len(ssspatch_sift_norm_img.ssspatch_train)}')
+print(f'Len val: {len(ssspatch_sift_norm_img.ssspatch_val)}')
+
+trainer.fit(model, datamodule=ssspatch_sift_norm_img)
