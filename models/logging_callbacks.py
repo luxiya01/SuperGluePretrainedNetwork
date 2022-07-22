99995954793bb0821cf384b03db96945305e26f1
Li Ling <liling@kth.se>
2022-07-22 13:08:29 +0200

    Refactor log_matches method

--- models/logging_callbacks.py
+++ models/logging_callbacks.py
@@ -1,4 +1,4 @@
-from typing import Optional, Any, List
+from typing import Optional, Any
 
 import numpy as np
 import pytorch_lightning as pl
@@ -27,11 +27,10 @@ class LogImagesCallback(pl.Callback):
         )
 
         # Log GT matches
-        matching_kps0, matching_kps1 = get_matching_keypoints_according_to_matches(matches=batch['groundtruth_match0'],
-                                                                                   keypoints0=batch['keypoints0'],
-                                                                                   keypoints1=batch['keypoints1'])
-        self._log_matches(trainer, batch, matching_kps0, matching_kps1, match_type='GT', stage='val',
-                          color=[1, 1, 0, 1])
+        gt_matching_kps0, gt_matching_kps1 = get_matching_keypoints_according_to_matches(
+            matches=batch['groundtruth_match0'],
+            keypoints0=batch['keypoints0'],
+            keypoints1=batch['keypoints1'])
 
         # Log correct predictions
         correct_pred_matches0 = torch.where(batch['groundtruth_match0'] == outputs['pred']['matches0'],
@@ -40,8 +39,6 @@ class LogImagesCallback(pl.Callback):
             matches=correct_pred_matches0,
             keypoints0=batch['keypoints0'],
             keypoints1=batch['keypoints1'])
-        self._log_matches(trainer, batch, correct_pred_kps0, correct_pred_kps1, match_type='Correct pred', stage='val',
-                          color=[0, 1, 0, 1])
 
         # Log incorrect predictions
         incorrect_pred_matches0 = torch.where(batch['groundtruth_match0'] != outputs['pred']['matches0'],
@@ -50,25 +47,29 @@ class LogImagesCallback(pl.Callback):
             matches=incorrect_pred_matches0,
             keypoints0=batch['keypoints0'],
             keypoints1=batch['keypoints1'])
-        self._log_matches(trainer, batch, incorrect_pred_kps0, incorrect_pred_kps1, match_type='Incorrect pred',
-                          stage='val',
-                          color=[1, 0, 0, 1])
 
-    def _log_matches(self, trainer: "pl.Trainer", batch: Any, matching_kps0: torch.Tensor, matching_kps1: torch.Tensor,
-                     match_type: str, stage: str = 'val', color: List = [0, 1, 0, 1]):
+        matches = {'GT': {'mkpts0': gt_matching_kps0, 'mkpts1': gt_matching_kps1, 'color': [1, 1, 0, 1]},
+                   'Incorrect predictions': {'mkpts0': incorrect_pred_kps0, 'mkpts1': incorrect_pred_kps1,
+                                             'color': [1, 0, 0, 1]},
+                   'Correct predictions': {'mkpts0': correct_pred_kps0, 'mkpts1': correct_pred_kps1,
+                                           'color': [0, 1, 0, 1]}
+                   }
+        self._log_matches(trainer, batch, matches, stage='val')
+
+    def _log_matches(self, trainer: "pl.Trainer", batch: Any, matches: dict, stage: str = 'val'):
         trainer.logger.log_image(
-            key=f'{stage}/matches/{match_type}',
+            key=f'{stage}/matches',
             images=[make_matching_plot_fast(batch['image0_norm'][0][0], batch['image1_norm'][0][0],
                                             batch['keypoints0'][0].numpy(),
                                             batch['keypoints1'][0].numpy(),
-                                            matching_kps0.numpy(),
-                                            matching_kps1.numpy(),
+                                            val['mkpts0'].numpy(),
+                                            val['mkpts1'].numpy(),
                                             # color = (r,g,b,a)
-                                            color=np.tile(color, (batch['keypoints0'].shape[1], 1)),
-                                            text='GT',
+                                            color=np.tile(val['color'], (batch['keypoints0'].shape[1], 1)),
+                                            text=key,
                                             show_keypoints=True
-                                            )],
+                                            ) for key, val in matches.items()],
             caption=[
-                f'{match_type} matches patches({batch["patch_id0"][0]}, {batch["patch_id1"][0]})'
-                f'nbr_matches = {len(matching_kps1)}']
+                f'{key} patches({batch["patch_id0"][0]}, {batch["patch_id1"][0]}) '
+                f'nbr_matches = {len(val["mkpts0"])}' for key, val in matches.items()]
         )
