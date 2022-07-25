4524dd887cded4d16330d008dd240b2641414d0b
Li Ling <liling@kth.se>
2022-07-25 14:06:43 +0200

    Modify image logging: log matches for train,test,val batches

--- models/logging_callbacks.py
+++ models/logging_callbacks.py
@@ -19,13 +19,31 @@ class LogImagesCallback(pl.Callback):
             batch_idx: int,
             dataloader_idx: int,
     ) -> None:
-        # Log patches
-        trainer.logger.log_image(
-            key='val/images',
-            images=[batch['image0_norm'].to(torch.float), batch['image1_norm'].to(torch.float)],
-            caption=[f'patch {batch["patch_id0"][0]}', f'patch {batch["patch_id1"][0]}']
-        )
+        self._log_images(trainer, outputs, batch, stage='val')
 
+    def on_train_batch_end(
+        self,
+        trainer: "pl.Trainer",
+        pl_module: "pl.LightningModule",
+        outputs: STEP_OUTPUT,
+        batch: Any,
+        batch_idx: int,
+        unused: int = 0,
+    ) -> None:
+        self._log_images(trainer, outputs, batch, stage='train')
+
+    def on_test_batch_end(
+        self,
+        trainer: "pl.Trainer",
+        pl_module: "pl.LightningModule",
+        outputs: Optional[STEP_OUTPUT],
+        batch: Any,
+        batch_idx: int,
+        dataloader_idx: int,
+    ) -> None:
+        self._log_images(trainer, outputs, batch, stage='test')
+
+    def _log_images(self, trainer: "pl.Trainer", outputs: Optional[STEP_OUTPUT], batch: Any, stage: str = 'val'):
         # Log GT matches
         gt_matching_kps0, gt_matching_kps1 = get_matching_keypoints_according_to_matches(
             matches=batch['groundtruth_match0'],
@@ -54,9 +72,10 @@ class LogImagesCallback(pl.Callback):
                    'Correct predictions': {'mkpts0': correct_pred_kps0, 'mkpts1': correct_pred_kps1,
                                            'color': [0, 1, 0, 1]}
                    }
-        self._log_matches(trainer, batch, matches, stage='val')
+        self._log_matches(trainer, batch, matches, stage=stage)
 
-    def _log_matches(self, trainer: "pl.Trainer", batch: Any, matches: dict, stage: str = 'val'):
+    @staticmethod
+    def _log_matches(trainer: "pl.Trainer", batch: Any, matches: dict, stage: str = 'val'):
         trainer.logger.log_image(
             key=f'{stage}/matches',
             images=[make_matching_plot_fast(batch['image0_norm'][0][0], batch['image1_norm'][0][0],
