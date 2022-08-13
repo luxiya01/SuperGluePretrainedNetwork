bffab1d4cd648f22243deab86afcb75d0ed4cc53
Li Ling <liling@kth.se>
2022-08-13 15:51:48 +0200

    Add is_noisy_keypoints0/1 to each data item to allow for separation between noisy and annotated keypoints

--- data/ssspatch_dataset.py
+++ data/ssspatch_dataset.py
@@ -48,27 +48,34 @@ class SSSPatchDataset(Dataset):
     def _add_noisy_keypoints_and_modify_noisy_gt_match(self, data) -> dict:
         # Add noisy keypoints to image0 data
         keypoints0_new_indices = self._generate_random_idx_for_annotated_kps(data['keypoints0'].shape[0])
-        keypoints0_noisy, gt_match0_noisy, scores0_noisy = self._add_noisy_keypoints(data['keypoints0'],
-                                                                                     data['gt_match0'],
-                                                                                     keypoints0_new_indices)
+        noisy_kps0_dict = self._add_noisy_keypoints(data['keypoints0'],
+                                                    data['gt_match0'],
+                                                    keypoints0_new_indices)
 
         # Add noisy keypoints to image1 data
         keypoints1_new_indices = self._generate_random_idx_for_annotated_kps(data['keypoints1'].shape[0])
-        keypoints1_noisy, gt_match1_noisy, scores1_noisy = self._add_noisy_keypoints(data['keypoints1'],
-                                                                                     data['gt_match1'],
-                                                                                     keypoints1_new_indices)
+        noisy_kps1_dict = self._add_noisy_keypoints(data['keypoints1'],
+                                                    data['gt_match1'],
+                                                    keypoints1_new_indices)
 
         # Update the indices in gt_match0_noisy and gt_match1_noisy to correspond to the noisy keypoint indices
-        gt_match0_noisy = np.where(gt_match0_noisy == NO_MATCH, gt_match0_noisy,
-                                      keypoints1_new_indices[gt_match0_noisy])
-        gt_match1_noisy = np.where(gt_match1_noisy == NO_MATCH, gt_match1_noisy,
-                                      keypoints0_new_indices[gt_match1_noisy])
-        return {'noisy_keypoints0': keypoints0_noisy, 'noisy_keypoints1': keypoints1_noisy,
-                'noisy_gt_match0': gt_match0_noisy, 'noisy_gt_match1': gt_match1_noisy,
-                'noisy_scores0': scores0_noisy, 'noisy_scores1': scores1_noisy}
+        gt_match0_noisy = np.where(noisy_kps0_dict['noisy_gt_match'] == NO_MATCH,
+                                   noisy_kps0_dict['noisy_gt_match'],
+                                   keypoints1_new_indices[noisy_kps0_dict['noisy_gt_match']])
+        gt_match1_noisy = np.where(noisy_kps1_dict['noisy_gt_match'] == NO_MATCH,
+                                   noisy_kps1_dict['noisy_gt_match'],
+                                   keypoints0_new_indices[noisy_kps1_dict['noisy_gt_match']])
+        return {'noisy_keypoints0': noisy_kps0_dict['noisy_kps'],
+                'noisy_keypoints1': noisy_kps1_dict['noisy_kps'],
+                'is_noisy_keypoints0': noisy_kps0_dict['is_noisy_kps'],
+                'is_noisy_keypoints1': noisy_kps1_dict['is_noisy_kps'],
+                'noisy_gt_match0': gt_match0_noisy,
+                'noisy_gt_match1': gt_match1_noisy,
+                'noisy_scores0': noisy_kps0_dict['noisy_scores'],
+                'noisy_scores1': noisy_kps1_dict['noisy_scores']}
 
     def _add_noisy_keypoints(self, annotated_kps: np.array, gt_match: np.array,
-                             annotated_kps_indices: np.array) -> (np.array, np.array, np.array):
+                             annotated_kps_indices: np.array) -> dict:
         # TODO: switch between adding random keypoints, SIFT or other keypoints!
 
         # Generate random keypoints
@@ -81,6 +88,10 @@ class SSSPatchDataset(Dataset):
         # Place annotated_kps at randomly generated indices in the output noisy_kps array
         noisy_kps[annotated_kps_indices, :] = annotated_kps
 
+        # Generate is_noisy_kps: boolean array where True = noisy kp, False = originally annotated kp
+        is_noisy_kps = np.ones(self.num_kps, dtype=bool)
+        is_noisy_kps[annotated_kps_indices] = 0
+
         # Generate detection confidence score for keypoints (random for random kps, 1 for annotated kps)
         noisy_scores = np.random.random(self.num_kps)
         noisy_scores[annotated_kps_indices] = 1
@@ -88,7 +99,8 @@ class SSSPatchDataset(Dataset):
         # Update gt_match to correspond to noisy_kps
         noisy_gt_match = np.ones(self.num_kps) * NO_MATCH
         noisy_gt_match[annotated_kps_indices] = gt_match
-        return noisy_kps.astype(int), noisy_gt_match.astype(int), noisy_scores.astype(float)
+        return {'noisy_kps': noisy_kps.astype(int), 'is_noisy_kps': is_noisy_kps,
+                'noisy_gt_match': noisy_gt_match.astype(int), 'noisy_scores': noisy_scores.astype(float)}
 
     def _generate_random_idx_for_annotated_kps(self, num_annotated_kps: int) -> np.array:
         # TODO: handle the case where num_annotated_kps > self.num_kps (random sample up to self.num_kps?)
@@ -124,7 +136,7 @@ class SSSPatchDataset(Dataset):
         data_torch['sss_waterfall_image0'] = data_torch['sss_waterfall_image0'].unsqueeze(dim=0)
         data_torch['sss_waterfall_image1'] = data_torch['sss_waterfall_image1'].unsqueeze(dim=0)
 
-        #TODO: add transforms to images (image0/1 = transformed versions of sss_waterfall_image0/1)
+        # TODO: add transforms to images (image0/1 = transformed versions of sss_waterfall_image0/1)
         data_torch['image0'] = data_torch['sss_waterfall_image0'].detach().clone()
         data_torch['image1'] = data_torch['sss_waterfall_image1'].detach().clone()
         return data_torch
@@ -135,7 +147,7 @@ def get_matching_keypoints_according_to_matches(matches, keypoints0, keypoints1)
     the corresponding keypoints to matching_kps1[i] according to the input matches array (which could be groundtruth
     or predictions)."""
     kps0_idx = torch.where(matches > NO_MATCH)
-    matching_kps0 = keypoints0[ kps0_idx]
+    matching_kps0 = keypoints0[kps0_idx]
 
     kps1_idx = matches[kps0_idx].to(int)
     matching_kps1 = keypoints1[kps1_idx]
