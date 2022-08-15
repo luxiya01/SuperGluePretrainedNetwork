c866572ce9258ac98162d49dd1087e777e0e5861
Li Ling <liling@kth.se>
2022-08-15 12:21:18 +0200

    Add more off-the-shelf feature extractors from Kornia library.

--- models/feature_extractor.py
+++ models/feature_extractor.py
@@ -22,6 +22,7 @@ class FeatureExtractor(pl.LightningModule):
             kps = torch.from_numpy(kps).reshape(batch_size, num_kps, 2)
         return feature.laf_from_center_scale_ori(kps)
 
+    # TODO: allow gradient to flow through the feature extraction modules...!?
     @torch.no_grad()
     def extract_features(self, image: torch.Tensor, kps: torch.Tensor) -> torch.Tensor:
         """Returns features with shape (batch_size, num_kps, descriptor_size)"""
@@ -46,10 +47,11 @@ class FeatureExtractor(pl.LightningModule):
 
 
 class SIFTFeatureExtractor(FeatureExtractor):
-    def __init__(self, patch_size: int = 41, num_ang_bins: int = 8, num_spatial_bins: int = 4, rootsift: bool = False,
+    def __init__(self, patch_size: int = 32, num_ang_bins: int = 8, num_spatial_bins: int = 4, rootsift: bool = False,
                  clipval: float = .2):
         """Output feature size = num_ang_bins * (num_spatial_bins^2)"""
         super(SIFTFeatureExtractor, self).__init__(patch_size_for_feature=patch_size)
+        self.output_dims = num_ang_bins * (num_spatial_bins ** 2)
         self.descriptor = feature.SIFTDescriptor(
             patch_size=patch_size,
             num_ang_bins=num_ang_bins,
@@ -60,7 +62,57 @@ class SIFTFeatureExtractor(FeatureExtractor):
 
 
 class HardNetFeatureExtractor(FeatureExtractor):
-    def __init__(self, pretrained: bool = False):
-        """Output feature size fixed to = 128"""
+    def __init__(self, pretrained: bool = True):
+        """Output feature size fixed to = 128. Can only compute patch_size of 32x32."""
         super(HardNetFeatureExtractor, self).__init__(patch_size_for_feature=32)
+        self.output_dims = 128
         self.descriptor = feature.HardNet(pretrained=pretrained)
+
+
+class HardNet8FeatureExtractor(FeatureExtractor):
+    def __init__(self, pretrained: bool = True):
+        """Output feature size fixed to = 128. Can only compute patch_size of 32x32."""
+        super(HardNet8FeatureExtractor, self).__init__(patch_size_for_feature=32)
+        self.output_dims = 128
+        self.descriptor = feature.HardNet8(pretrained=pretrained)
+
+
+class MKDDFeatureExtractor(FeatureExtractor):
+    def __init__(self, patch_size: int = 32, kernel_type: str = 'concat',
+                 whitening: str = 'pcawt', training_set='liberty', output_dims: int = 128):
+        super(MKDDFeatureExtractor, self).__init__(patch_size_for_feature=patch_size)
+        self.output_dims = output_dims
+        self.descriptor = feature.MKDDescriptor(patch_size=patch_size,
+                                                kernel_type=kernel_type,
+                                                whitening=whitening,
+                                                training_set=training_set,
+                                                output_dims=output_dims)
+
+
+class HyNetFeatureExtractor(FeatureExtractor):
+    def __init__(self, pretrained: bool = False, is_bias: bool = True,
+                 is_bias_FRN: bool = True, output_dims: int = 128, drop_rate: float = .3):
+        """Can only compute patch_size of 32x32."""
+        super(HyNetFeatureExtractor, self).__init__(patch_size_for_feature=32)
+        self.output_dims = output_dims
+        self.descriptor = feature.HyNet(pretrained=pretrained,
+                                        is_bias=is_bias,
+                                        is_bias_FRN=is_bias_FRN,
+                                        dim_desc=output_dims,
+                                        drop_rate=drop_rate)
+
+
+class TFeatFeatureExtractor(FeatureExtractor):
+    def __init__(self, pretrained: bool = True):
+        """Output feature size fixed to = 128. Can only compute patch_size of 32x32."""
+        super(TFeatFeatureExtractor, self).__init__(patch_size_for_feature=32)
+        self.output_dims = 128
+        self.descriptor = feature.TFeat(pretrained=pretrained)
+
+
+class SOSNetFeatureExtractor(FeatureExtractor):
+    def __init__(self, pretrained: bool = True):
+        """Output feature size fixed to = 128. Can only compute patch_size of 32x32."""
+        super(SOSNetFeatureExtractor, self).__init__(patch_size_for_feature=32)
+        self.output_dims = 128
+        self.descriptor = feature.SOSNet(pretrained=pretrained)
