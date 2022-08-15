014106c5ef3ebade435f45851b3b8a12d721f58d
Li Ling <liling@kth.se>
2022-08-15 17:41:49 +0200

    Add transform: column wise normalization.

---
+++ data/image_transforms.py
@@ -0,0 +1,14 @@
+import torch
+
+
+class ColumnwiseNormalization:
+    """Normalize input image by dividing each column with the column mean and clipping
+    the resulting values between (0, a_max)."""
+    def __init__(self, a_max):
+        self.a_max = a_max
+
+    def __call__(self, image):
+        # image shape = (1, H (num_pings), W (num_bins))
+        col_mean = image.mean(axis=1)
+        image = torch.nan_to_num(image/col_mean)
+        return torch.clip(image, min=0., max=self.a_max)
