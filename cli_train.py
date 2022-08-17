742fee109e1ce6b59764b603ac82594168e7ecee
Li Ling <liling@kth.se>
2022-08-17 14:19:39 +0200

    Ignore PL config file and don't save it for now.

--- cli_train.py
+++ cli_train.py
@@ -3,5 +3,8 @@ from pytorch_lightning.cli import LightningCLI
 from data.ssspatch_datamodule import SSSPatchDataModule
 from models.matching_train import MatchingTrain
 
+# N.B the saved PL config is overwritten every time a new experiment is started!
+# i.e. check out wandb/[run]/files/config.yaml for actual experiment config!
 cli = LightningCLI(MatchingTrain, SSSPatchDataModule,
-                   save_config_filename='config_cli.yaml')
+                   save_config_filename='config_cli.yaml',
+                   save_config_overwrite=True)
