9912aae3fb0893bafdea78940aae689eca1b6cff
Li Ling <liling@kth.se>
2022-08-17 11:08:40 +0200

    Configure wandblogger correctly using YAML file.

--- cli_train.py
+++ cli_train.py
@@ -3,4 +3,5 @@ from pytorch_lightning.cli import LightningCLI
 from data.ssspatch_datamodule import SSSPatchDataModule
 from models.matching_train import MatchingTrain
 
-cli = LightningCLI(MatchingTrain, SSSPatchDataModule)
+cli = LightningCLI(MatchingTrain, SSSPatchDataModule,
+                   save_config_filename='config_cli.yaml')
